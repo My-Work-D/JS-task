16793a4b1f2f5effd9ad2eabaf779cfde6f1f300
ApsaraWitharana <asachini095@gmail.com>
2024-09-28 13:16:15 +0530

    update- add arry task new

--- js-task/task1.js
+++ js-task/task1.js
@@ -22,3 +22,37 @@ let array2 = array1
  // out-put -
  // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10][1, 10, 2, 3, 4, 5,  6, 7, 8, 9]
  console.log("==============")
+
+ let array = [1,2,3,4,5,6,7,8,9,10]
+ function checkNum(num){
+ return num >= 4;
+ }
+ let newArray1 = [array.map((item) => {
+  return item
+ })].filter(checkNum)
+
+ let newArray2 = array.filter(checkNum).map((item) => {
+  return item
+ })
+
+ console.log(newArray1)
+ console.log(newArray2)
+
+ // out-put - [][4, 5,  6, 7, 8, 9, 10]
+ console.log("==================")
+
+ let array3 = [1,2,3,4,5,6,7,8,9,10]
+ function checkNum(num){
+ return num > 4;
+ }
+
+ let newArray = [array3.filter(checkNum).map((item,index) => {
+  return item - array3[index]
+ }).reduce((acc,curr) => {
+  return acc + curr
+ })]
+
+ console.log(newArray)
+
+ // out-put- [ 24 ]
+ console.log("====================")
